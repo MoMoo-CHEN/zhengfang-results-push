fbf89a02a128e644ec890fd63ea52269f608eec9
MoMoo-CHEN <3070717049@qq.com>
2024-06-23 11:53:31 +0800

    Update config.php

--- lib/config.php
+++ lib/config.php
@@ -11,7 +11,7 @@ return [
     //不知道 gnmkdm
     'URL' => 'https://jwglxt.nmukd.edu.cn/jwglxt/xtgl/login_slogin.html/jwglxt/cjcx/cjcx_cxDgXscj.html?doType=query&gnmkdm=N305005',
     //请将“教务系统域名”改成你的正方教务系统域名
-
+AT_xWvcvmiQbp4MumOql3k7rWHDU0Ykjtr5
     //哪个学年的成绩
     //2020 表示 2020~2021学年度
     'XNM' => 2023,
